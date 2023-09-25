12aee4054cdc7563f3bba2b76580ca99a2550f38
Dominguez Otoniel <otonielex77c@gmail.com>
2023-09-25 15:33:33 -0600

    nuevo curl para ver el PDF

--- application/models/Books_model.php
+++ application/models/Books_model.php
@@ -22,6 +22,26 @@
             curl_close($curl);
             return json_decode($response,true);
         }
+
+        public function get_pdfEstimate($token,$id){
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://www.zohoapis.com/books/v3/estimates/pdf?organization_id=737962647&estimate_ids='.$id,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+                CURLOPT_HTTPHEADER => array(
+                    'Authorization: Zoho-oauthtoken '.$token,
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            return json_decode($response,true);
+        }
         
         public function create_estimates($token,$data){
             $curl = curl_init();
